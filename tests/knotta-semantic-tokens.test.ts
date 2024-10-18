159f8cb43c78ab41d884d341e82857650554595c
Katsuyuki Oeda <katsu.amagramer@gmail.com>
2024-10-18 15:40:13 +0900

    💪

--- tests/knotta-semantic-tokens.test.ts
+++ tests/knotta-semantic-tokens.test.ts
@@ -17,7 +17,50 @@ export const knottaSemanticTokensTest = test("knotta semantic tokens test").do(
     });
 
     assert.deepStrictEqual(errors, []);
-    console.log(result.semanticTokens);
-    // assert.deepStrictEqual(result.semanticTokens, []);
+    // console.log(result.semanticTokens);
+    assert.deepStrictEqual(result.semanticTokens, [
+      { type: "import", length: 6, line: 0, inlineIndex: 0 },
+      { type: "string", length: 25, line: 0, inlineIndex: 7 },
+      { type: "type", length: 4, line: 2, inlineIndex: 0 },
+      { type: "type-name", length: 4, line: 2, inlineIndex: 5 },
+      { type: "type-decorator", length: 9, line: 3, inlineIndex: 4 },
+      { type: "prop-name", length: 2, line: 4, inlineIndex: 4 },
+      { type: "prop-type-name", length: 4, line: 4, inlineIndex: 8 },
+      { type: "prop-name", length: 8, line: 5, inlineIndex: 4 },
+      { type: "prop-type-name", length: 6, line: 5, inlineIndex: 14 },
+      { type: "prop-decorator", length: 7, line: 5, inlineIndex: 21 },
+      { type: "prop-name", length: 12, line: 6, inlineIndex: 4 },
+      { type: "prop-type-name", length: 6, line: 6, inlineIndex: 18 },
+      { type: "prop-decorator", length: 7, line: 6, inlineIndex: 25 },
+      { type: "type", length: 4, line: 9, inlineIndex: 0 },
+      { type: "type-name", length: 7, line: 9, inlineIndex: 5 },
+      { type: "prop-name", length: 2, line: 10, inlineIndex: 4 },
+      { type: "prop-type-name", length: 4, line: 10, inlineIndex: 8 },
+      { type: "prop-name", length: 9, line: 11, inlineIndex: 4 },
+      { type: "prop-type-name", length: 6, line: 11, inlineIndex: 15 },
+      { type: "prop-name", length: 8, line: 12, inlineIndex: 4 },
+      { type: "prop-type-name", length: 6, line: 12, inlineIndex: 14 },
+      { type: "type", length: 4, line: 15, inlineIndex: 0 },
+      { type: "type-name", length: 5, line: 15, inlineIndex: 5 },
+      { type: "prop-name", length: 2, line: 16, inlineIndex: 4 },
+      { type: "prop-type-name", length: 4, line: 16, inlineIndex: 8 },
+      { type: "prop-name", length: 9, line: 17, inlineIndex: 4 },
+      { type: "prop-type-name", length: 8, line: 17, inlineIndex: 15 },
+      { type: "prop-name", length: 6, line: 18, inlineIndex: 4 },
+      { type: "prop-type-name", length: 4, line: 18, inlineIndex: 12 },
+      { type: "prop-ref", length: 2, line: 18, inlineIndex: 17 },
+      { type: "type", length: 4, line: 21, inlineIndex: 0 },
+      { type: "type-name", length: 4, line: 21, inlineIndex: 5 },
+      { type: "prop-name", length: 2, line: 22, inlineIndex: 4 },
+      { type: "prop-type-name", length: 4, line: 22, inlineIndex: 8 },
+      { type: "prop-name", length: 5, line: 23, inlineIndex: 4 },
+      { type: "prop-type-name", length: 6, line: 23, inlineIndex: 11 },
+      { type: "prop-name", length: 8, line: 24, inlineIndex: 4 },
+      { type: "prop-type-name", length: 8, line: 24, inlineIndex: 14 },
+      { type: "prop-decorator", length: 9, line: 24, inlineIndex: 23 },
+      { type: "prop-name", length: 8, line: 25, inlineIndex: 4 },
+      { type: "prop-type-name", length: 4, line: 25, inlineIndex: 14 },
+      { type: "prop-ref", length: 2, line: 25, inlineIndex: 19 },
+    ]);
   }
 );
